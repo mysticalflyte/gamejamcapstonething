76f7cd7974aeb6e3c9fffc6d8ee47a36cd33976b
Sarah <skr.biscuit@gmail.com>
2020-02-01 16:25:35 -0600

    teacher patrol working

--- Custodial Conundrum/Assets/Scripts/TeacherView.cs
+++ Custodial Conundrum/Assets/Scripts/TeacherView.cs
@@ -4,22 +4,54 @@ using UnityEngine;
 
 public class TeacherView : MonoBehaviour
 {
+    public float detectionAngle = 45.0f;
+    private bool detecting = false;
+    private RaycastHit hit;
+    private Transform player;
+    int layerMask = 1 << 8;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (detecting) {
+            checkSight();
+        }
+    }
+
+    private void checkSight() {
+        Vector3 playerdir = (player.position - transform.position).normalized;
+        Vector3 teacherdir = transform.forward;
+        if (Mathf.Abs(Vector3.Angle(playerdir, teacherdir)) <= detectionAngle)
+        {
+            if (Physics.Raycast(transform.position, playerdir, out hit)) {
+                if (hit.collider.gameObject.tag == "Player")
+                {
+                    Debug.DrawLine(player.position, transform.position, Color.green);
+                    Debug.Log("In Angle!");
+                }
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player") {
             Debug.Log("Player Detected!");
+            player = other.transform;
+            detecting = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player") {
+            player = null;
+            detecting = false;
         }
     }
 }
