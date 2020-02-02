f203cf7bf7990f9783933c38109a53217ff7d6ef
Sarah <skr.biscuit@gmail.com>
2020-02-01 18:11:09 -0600

    object interaction

---
+++ Custodial Conundrum/Assets/Scripts/ObjectInteract.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectInteract : MonoBehaviour
+{
+    bool interactable;
+    // Start is called before the first frame update
+    void Start()
+    {
+        interactable = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            transform.parent.GetComponent<Rigidbody>().useGravity = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player") {
+            interactable = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            interactable = false;
+        }
+    }
+}
