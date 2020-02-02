d6b07064d308d2a87834d445e5ba4600f9d173f6
Jenny Liu <jennyliu98@utexas.edu>
2020-02-02 16:18:15 -0600

    Got listening range on student AI working sort of

---
+++ Custodial Conundrum/Assets/Scripts/CloseRangeDetection.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseRangeDetection : MonoBehaviour
+{
+    StudentListenRange studentEnemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        studentEnemy = transform.parent.GetComponent<StudentListenRange>();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Player Detected in CLOSE RANGE!");
+            studentEnemy.SetCloseRange(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Player Exited from CLOSE RANGE!");
+            studentEnemy.SetCloseRange(false);
+        }
+    }
+}
